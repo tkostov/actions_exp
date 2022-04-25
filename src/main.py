ff57ef94b8fc710a25bb779ef91b5cf50de0ae9e
Todor Kostov <kostov@reasonance.de>
2022-04-25 06:59:09 +0200

    exp2

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-def some(abc:str) -> None:
+"""
+Main module to explain what are the expectations.
+We are going to destroi it frist for the demo.
+
+"""
+def some(abc: str) -> None:
+    """
+    Some function description. No function passes flake8.
+    """
     print(abc)
 
+
 def second() -> None:
+    """
+    Some function description. No function passes flake8.
+    """
     print('Hello World')
